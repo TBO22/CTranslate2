7b390fb6ea29a8eeb65381881b0e39b7285fa3ec
Talha Bin Omar <155762939+TBO22@users.noreply.github.com>
2026-09-20 16:14:13 +0500

    Preserve CPU array interface error message

--- python/cpp/storage_view.cc
+++ python/cpp/storage_view.cc
@@ -181,6 +181,9 @@ namespace ctranslate2 {
         })
 
         .def_property_readonly("__cuda_array_interface__", [](const StorageView& view) {
+          if (view.device() == Device::CPU)
+            throw py::attribute_error("Cannot get __cuda_array_interface__ when the StorageView "
+                                      "is viewing a CPU array");
           if (view.device() != Device::CUDA)
             throw py::attribute_error("Cannot get __cuda_array_interface__ when the StorageView "
                                       "is not viewing a CUDA array");
